24dac66f5bdb311556b2db6fb21774c1dd065869
avbranco <avbranco@gmail.com>
2010-09-20 17:54:55 -0300

    devise for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,11 @@
 
 class User < ActiveRecord::Base
-  validates_presence_of :name
-  validates_uniqueness_of :name
-  
-  attr_accessor :password_confirmation
-  validates_confirmation_of :password
-  
-  def validate
-    errors.add_to_base("Missing password") if hashed_password.blank?
-  end
-  
-  private
-  
-  def self.encrypted_password(password,salt)
-    
-  end
-  
-end
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,36 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100914183828) do
+ActiveRecord::Schema.define(:version => 20100917190155) do
+
+  create_table "books", :force => true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "hashed_passoword"
-    t.string   "salt"
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
