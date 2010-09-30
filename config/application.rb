2790c39083893ae7c63ff42961b2cd4a291764a5
Pericles Dias <pericles.dias@gmail.com>
2010-09-30 15:29:08 -0300

    Changing from prototype to jquery

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Booklog
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    
+    # JavaScript files you want as :defaults (application.js is always included).
+    config.action_view.javascript_expansions[:defaults] = %w(http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js rails)
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
