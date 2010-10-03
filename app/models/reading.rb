68ff899fefe10aa7fe05163382fca7e081e2cbf5
avbranco <avbranco@gmail.com>
2010-10-02 23:45:37 -0300

    save a reading

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,4 +1,7 @@
 class Reading < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
+  
+  validates_presence_of :user
+  validates_presence_of :book
 end
--- test/unit/reading_test.rb
+++ test/unit/reading_test.rb
@@ -1,6 +1,21 @@
 require 'test_helper'
 
 class ReadingTest < ActiveSupport::TestCase
+  
+  setup do
+    @reading = Reading.new
+  end
+  
   should belong_to :user
   should belong_to :book
+  
+  should validate_presence_of :user
+  should validate_presence_of :book
+  
+  test "should save reading" do
+    @reading.book = Factory(:book_one)
+    @reading.user = Factory(:user)
+    assert @reading.save, "Didn't save the reading"
+  end
+  
 end
